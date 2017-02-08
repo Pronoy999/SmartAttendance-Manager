8ea55221a01fc836b70c76b37644e12cb05efc36
Shubham <shubho.193@gmail.com>
2017-02-09 00:17:29 +0530

    Add files via upload

---
+++ app/src/androidTest/java/com/globsynproject/smartattendancemanager/Constant.java
@@ -0,0 +1,20 @@
+package com.globsynproject.smartattendancemanager;
+
+/**
+ * This is the abstract class to declare all the constants of the Data bases and file and other classes.
+ */
+
+public abstract class Constant {
+    public static final String DATABASE_NAME="StudentDataBase.db";
+    public static final String TABLE_NAME="Student";
+    public static final String FILE_NAME="temp.dat";
+    public   static final int DATABASE_VERSION = 1;
+    public static final String UID = "_id";
+    public static final String NAME = "Name";
+    public static final String BSSID = "BSSID";
+    public static final String ROLL_NUMBER = "ROLL NUMBER";
+    public static final String ATTENDANCE = "ATTENDANCE";
+    public static final String SSID = "SSID";
+    public static final String PASSWORD = "PASSWORD";
+
+}
