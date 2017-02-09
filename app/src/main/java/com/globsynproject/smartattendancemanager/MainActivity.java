8f86b4acd6051958707eb7ecc7142e71922fba99
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2017-02-09 13:13:41 +0530

    Main Activity written and BUILD Version checked for permissions.

--- app/src/main/java/com/globsynproject/smartattendancemanager/MainActivity.java
+++ app/src/main/java/com/globsynproject/smartattendancemanager/MainActivity.java
@@ -1,13 +1,62 @@
 package com.globsynproject.smartattendancemanager;
 
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 /**This is the first activity to be displayed.*/
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView teacher;
+    private ImageView student;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M) {
+            if (!Settings.System.canWrite(this)) {
+                Intent i = new Intent().setAction(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                this.startActivity(i);
+            }
+        }
+        teacher=(ImageView) findViewById(R.id.teacher);
+        student=(ImageView) findViewById(R.id.student);
+        teacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToTeacher();
+            }
+        });
+        teacher.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Message.toastMessage(getApplicationContext(),"Teacher's Login!","");
+                return false;
+            }
+        });
+        student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToStudent();
+            }
+        });
+        student.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Message.toastMessage(getApplicationContext(),"Student's Login!","");
+                return false;
+            }
+        });
+    }
+    private void goToTeacher(){
+        Intent intent=new Intent(MainActivity.this,TeacherActivity.class);
+        startActivity(intent);
+    }
+    private void goToStudent(){
+        Intent intent=new Intent(MainActivity.this,TeacherActivity.class);
+        startActivity(intent);
     }
 }
